ed7acc53994c0d06ef3699a2e651134fe09bc60d
denisfilho <denisferreira93@hotmail.com>
2023-08-08 23:50:15 -0300

    Creating the Student Class

---
+++ src/Student.ts
@@ -0,0 +1,29 @@
+import { User } from "./User";
+
+class Stundent extends User{
+    private class_name:string;
+    private registration_number:string;
+
+    constructor(id:number, name:string, class_name:string, registration_number:string){
+        super(id, name);          
+        this.class_name = class_name;
+        this.registration_number = registration_number;
+        
+    }
+
+    getClassName(){
+        return this.class_name;
+    }
+
+    setClassName(class_name:string){
+        this.class_name = class_name;
+    }
+
+    getRegistratioNunmber(){
+        return this.registration_number;
+    }
+
+    setRegistratioNunmber(registration_number:string){
+        this.registration_number = registration_number;
+    }
+}
